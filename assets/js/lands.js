3bb4db951082f5eb2842eec91ed983b40b769a7d
mamun <mamunopstel@gmail.com>
2024-10-01 17:11:53 +0600

    second first commit before final touch

--- assets/js/lands.js
+++ assets/js/lands.js
@@ -7,7 +7,7 @@ const cardsData = [
     state: "NC",
     size: "3.45 Acres",
     price: "$24,500",
-    detailsLink: "/",
+    detailsLink: "/assets/pages/details.html",
   },
   {
     imgSrc: "/assets/images/background-images/abadimage.jpg",
@@ -59,6 +59,66 @@ const cardsData = [
     price: "$24,500",
     detailsLink: "/",
   },
+  {
+    imgSrc: "/assets/images/background-images/abadimage.jpg",
+    title:
+      "7 Build-ready 3.45-acre lot in the desirable Ceda Creek Mountain, Rutherford Country, NC!",
+    status: "Available",
+    state: "NC",
+    size: "3.45 Acres",
+    price: "$24,500",
+    detailsLink: "/",
+  },
+  {
+    imgSrc: "/assets/images/background-images/abadimage.jpg",
+    title:
+      "8 Build-ready 3.45-acre lot in the desirable Ceda Creek Mountain, Rutherford Country, NC!",
+    status: "Available",
+    state: "NC",
+    size: "3.45 Acres",
+    price: "$24,500",
+    detailsLink: "/",
+  },
+  {
+    imgSrc: "/assets/images/background-images/abadimage.jpg",
+    title:
+      "9 Build-ready 3.45-acre lot in the desirable Ceda Creek Mountain, Rutherford Country, NC!",
+    status: "Available",
+    state: "NC",
+    size: "3.45 Acres",
+    price: "$24,500",
+    detailsLink: "/",
+  },
+  {
+    imgSrc: "/assets/images/background-images/abadimage.jpg",
+    title:
+      "10 Build-ready 3.45-acre lot in the desirable Ceda Creek Mountain, Rutherford Country, NC!",
+    status: "Available",
+    state: "NC",
+    size: "3.45 Acres",
+    price: "$24,500",
+    detailsLink: "/",
+  },
+  {
+    imgSrc: "/assets/images/background-images/abadimage.jpg",
+    title:
+      "11 Build-ready 3.45-acre lot in the desirable Ceda Creek Mountain, Rutherford Country, NC!",
+    status: "Available",
+    state: "NC",
+    size: "3.45 Acres",
+    price: "$24,500",
+    detailsLink: "/",
+  },
+  {
+    imgSrc: "/assets/images/background-images/abadimage.jpg",
+    title:
+      "12 Build-ready 3.45-acre lot in the desirable Ceda Creek Mountain, Rutherford Country, NC!",
+    status: "Available",
+    state: "NC",
+    size: "3.45 Acres",
+    price: "$24,500",
+    detailsLink: "/",
+  },
 ];
 
 let currentIndex = 0;
@@ -69,7 +129,7 @@ function renderCards() {
   cardsContainer.innerHTML = "";
 
   // Get the current set of 2 cards
-  const cardsToShow = cardsData.slice(currentIndex, currentIndex + 4);
+  const cardsToShow = cardsData.slice(currentIndex, currentIndex + 6);
 
   // Loop through the cards and render them inside a col
   cardsToShow.forEach((cardData) => {
@@ -85,7 +145,7 @@ function renderCards() {
       </div>
       <div class="container text-box">
         <p class="title">${cardData.title}</p>
-        <div class="row row-cols-2">
+        <div class="row row-cols-2 g-2">
           <div class="col"><span>Status: </span>${cardData.status}</div>
           <div class="col"><span>State: </span>${cardData.state}</div>
           <div class="col"><span>Size: </span>${cardData.size}</div>
@@ -107,7 +167,7 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
     // Move to the next set of cards
-    currentIndex += 4;
+    currentIndex += 6;
 
     // If we reach the end of the array, wrap back to the beginning
     if (currentIndex >= cardsData.length) {
@@ -122,11 +182,11 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
     // Move to the previous set of cards
-    currentIndex -= 4;
+    currentIndex -= 6;
 
     // If we go below the beginning, wrap to the end
     if (currentIndex < 0) {
-      currentIndex = cardsData.length - 2;
+      currentIndex = cardsData.length - 6;
     }
 
     renderCards();
